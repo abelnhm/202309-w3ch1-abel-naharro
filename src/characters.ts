401fbebe01b6ef402163bacbafd0e5e08be73ebc
Abel Naharro <abelnhm92@gmail.com>
2023-10-23 21:39:10 +0200

    Add info to html

--- src/characters.ts
+++ src/characters.ts
@@ -6,6 +6,6 @@ import { Advisor } from './advisor';
 const joffrey = new King('Joffrey', 'Baratheon', 16, 2);
 const daenerys = new Fighter('Daenerys', 'Targaryen', 30, 'Dragones', 10);
 const bronn = new Squire('Bronn', 'AguasNegras', 54, 0);
-const tyrion = new Advisor('Tyrion', 'Lannister', 40, 'daenerys');
+const tyrion = new Advisor('Tyrion', 'Lannister', 40, 'Daenerys');
 
 export const characters = [joffrey, daenerys, bronn, tyrion];
--- src/main.ts
+++ src/main.ts
@@ -7,4 +7,42 @@ if (appElement === null) throw new Error('Critical Error.');
 
 console.log(characters);
 
-appElement.innerHTML = characters[0].phrase();
+appElement.innerHTML += `<ul class="characters-list row list-unstyled">`;
+
+for (const iterator of characters) {
+  appElement.innerHTML += `<li class="character col">
+        <div class="card character__card">
+          <img src="img/no-one.jpg" alt="Nombre y familia del personaje" class="character__picture card-img-top" />
+          <div class="card-body">
+            <h2 class="character__name card-title h4"> ${iterator.name} </h2>
+            <div class="character__info">
+              <ul class="list-unstyled">
+                <li>Edad: X años</li>
+                <li>
+                  Estado:
+                  <i class="fas fa-thumbs-down"></i>
+                  <i class="fas fa-thumbs-up"></i>
+                </li>
+              </ul>
+            </div>
+            <div class="character__overlay">
+              <ul class="list-unstyled">
+                <li>Años de reinado: X</li>
+                <li>Arma: XXX</li>
+                <li>Destreza: X</li>
+                <li>Peloteo: X</li>
+                <li>Asesora a: X</li>
+                <li>Sirve a: X</li>
+              </ul>
+              <div class="character__actions">
+                <button class="character__action btn">habla</button>
+                <button class="character__action btn">muere</button>
+              </div>
+            </div>
+          </div>
+          <i class="emoji"></i>
+        </div>
+      </li>`;
+}
+
+appElement.innerHTML += `</ul>`;
