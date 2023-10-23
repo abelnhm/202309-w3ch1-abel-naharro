1afdfa214df04b25b69ea9686a065469e26df720
Abel Naharro <abelnhm92@gmail.com>
2023-10-23 21:10:18 +0200

    Add classes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import './scss/main.scss';
+import { characters } from './characters';
 
 const appElement = document.querySelector<HTMLDivElement>('#app');
 
 if (appElement === null) throw new Error('Critical Error.');
 
-appElement.innerHTML = 'Index';
+console.log(characters);
+
+appElement.innerHTML = characters[0].phrase();
